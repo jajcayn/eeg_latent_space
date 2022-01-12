ce1970c37897ef87d6596b919ae4831983734328
Nikola Jajcay <nikola.jajcay@gmail.com>
2022-01-13 00:27:37 +0100

    crop 250 for all and more resources for meta to avoid exceeded walltime

--- submit_all_jobs.sh
+++ submit_all_jobs.sh
@@ -9,7 +9,12 @@ for datatype in "EC" "EO"
 do
     for decomptype in "kmeans" "AAHC" "TAAHC" "PCA" "ICA" "hmm"
     do
-        qsub -l walltime=24:0:0 -q default@meta-pbs.metacentrum.cz -l select=1:ncpus=30:mem=80gb:scratch_local=10gb -v target=$target,pyscript=$pyscript,decomptype=$decomptype,datatype=$datatype meta_runner.sh
+        if [ $decomptype == "AAHC" ] || [ $decomptype == "TAAHC" ]; then
+            comp_time=48
+        else
+            comp_time=24
+        fi
+        qsub -l walltime=$comp_time:0:0 -q default@meta-pbs.metacentrum.cz -l select=1:ncpus=32:mem=120gb:scratch_local=10gb -v target=$target,pyscript=$pyscript,decomptype=$decomptype,datatype=$datatype meta_runner.sh
     done
 done
 
@@ -22,7 +27,7 @@ done
 #     do
 #         for surrtype in "FT" "AAFT" "IAAFT" "shuffle"
 #         do
-#             qsub -l walltime=24:0:0 -q default@meta-pbs.metacentrum.cz -l select=1:ncpus=30:mem=80gb:scratch_local=10gb -v target=$target,pyscript=$pyscript,decomptype=$decomptype,datatype=$datatype,surrtype=$surrtype meta_runner.sh
+#             qsub -l walltime=24:0:0 -q default@meta-pbs.metacentrum.cz -l select=1:ncpus=32:mem=120gb:scratch_local=10gb -v target=$target,pyscript=$pyscript,decomptype=$decomptype,datatype=$datatype,surrtype=$surrtype meta_runner.sh
 #         done
 #     done
 # done
